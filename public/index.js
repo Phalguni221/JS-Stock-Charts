ef728fd5972a20ebaa6ea2c5d3017f7cd2807c34
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2021-11-21 17:01:40 -0500

    Update index.js

--- public/index.js
+++ public/index.js
@@ -16,49 +16,52 @@ function getColor(stock){
 async function main() {
 
     const timeChartCanvas = document.querySelector('#time-chart');
-    const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
-    const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+//     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
+//     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
-   let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,%20MSFT,%20DIS,%20BNTX&interval=1min&apikey=156e07f34dd049f8a78d78d99fb43c7e')
+//    let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,%20MSFT,%20DIS,%20BNTX&interval=1min&apikey=156e07f34dd049f8a78d78d99fb43c7e')
    
-    let result = await response.JSON()
+//     let result = await response.JSON()
 
 const { GME, MSFT, DIS, BNTX } = mockData;
 
 const stocks = [GME, MSFT, DIS, BNTX];
 
-stocks.forEach( stock => stock.values.reverse())
+stocks.forEach(stock => stock.values.reverse())
 
     new Chart(timeChartCanvas.getContext('2d'), {
         type: 'line',
         data: {
             labels: stocks[0].values.map(value => value.datetime),
-            datasets: stocks.map( stock => ({
-                label: stock.meta.symbol,
+            datasets: stocks.map(stock => ({
+                label: stock.meta.symtbol,
                 data: stock.values.map(value => parseFloat(value.high)),
-                backgroundColor:  getColor(stock.meta.symbol),
-                borderColor: getColor(stock.meta.symbol),
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol)
             }))
         }
     });
 
-    new Chart(highestPriceChartCanvas.getContext('2d'), {
+    new Chart(timeChartCanvas.getContext('2d'), {
         type: 'bar',
         data: {
-        labels: stocks.map(stock => stock.meta.symbol),
-            datasets: ({
-                label: 'HighestPrice',
-                backgroundColor: stocks.map(stock => (
-                getColor(stock.meta.symbol)
-                )),
-                borderColor: stocks.map(stock => (
-                getColor(stock.meta.symbol)
-                )),
-                data: stocks.map(stock => (findHighestPrice(stock.values)
-                ))
-        })
+            labels: stocks.map(stock => stock.meta.symbol),
+            datasets: [{
+                label: "The Highest Stock Price",
+                data: stocks.map(stock => highestPrice(stock.values)),
+                backgroundColor: stocks.map(stock => getColor(stock.meta.symbol)),
+                borderColor: stocks.map(stock => getColor(stock.meta.symbol))
+             }]
+        }
+    });
+}
+function  highestPrice(values) {
+    let highestvalue = 0;
+   values.forEach(value=> {
+    if (parseFloat(value.high) > highestvalue) {
+        highestvalue= value.high
     }
-});
-
+   })
+   return highestvalue;
 }
 main()
